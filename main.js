fc16529c11e0791b156ef02e9d044623261a58ed
Tomy <tomasgoizuetadp@gmail.com>
2023-05-15 17:09:36 +0200

    button

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ console.log("Javascript Works!");
 
 let role = document.querySelector('.role');
 
-let width = role.offsetWidth;
+let width = role.clientWidth;
 document.querySelector('.bio').style.width = width + 'px';
+document.querySelector('.cta').style.width = width/2 + 'px';
