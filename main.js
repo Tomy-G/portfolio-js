5852526bdd5ea7d15639b27ddb1b94b2755c8a73
Tomy <tomasgoizuetadp@gmail.com>
2023-05-16 15:08:51 +0200

    button

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ let role = document.querySelector('.role');
 
 let width = role.clientWidth;
 document.querySelector('.bio').style.width = width + 'px';
+document.querySelector('.buttons').style.width = width + 'px';
 document.querySelector('.cta').style.width = width/2 + 'px';
+
