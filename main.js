119ac957bbf8ea554b22ed4a78b14fb00e9cc1cc
Tomás <tomasgoizuetadp@gmail.com>
2023-06-03 13:55:02 +0200

    CSS good practices

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 console.log("Javascript Works!");
-
-let role = document.querySelector('.role');
-
-let width = role.clientWidth;
-document.querySelector('.bio').style.width = width + 'px';
-document.querySelector('.buttons').style.width = width + 'px';
-document.querySelector('.cta').style.width = width/2 + 'px';
-
